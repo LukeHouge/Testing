92fda6ff866781b67a76dc4a2acb597fc42f9f9f
Luke Houge <lukehouge@gmail.com>
2018-11-30 11:48:29 -0600

    added addition function in html

--- add.js
+++ add.js
@@ -4,7 +4,7 @@ operation.sub = function(value1, value2) {
     return value1 - value2;
 }
 operation.add = function(value1, value2) {
-    return value1 + value2;
+    return parseFloat(value1) + parseFloat(value2);
 }
 operation.div = function(value1, value2) {
     return value1 / value2;
